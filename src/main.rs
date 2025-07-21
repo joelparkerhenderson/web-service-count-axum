16e545879afbfc31124c5a4b7c55af2163558611
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-07-21 16:52:28 +0100

    Add env var

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 //! # Web service count axum
-//! 
+//!
 //! **[documentation](https://docs.rs/web-service-count-axum/)**
 //! •
 //! **[source](https://github.com/joelparkerhenderson/web-service-count-axum/)**
@@ -11,42 +11,56 @@
 //! **[email](mailto:joel@joelparkerhenderson.com)**
 //!
 //! Web service that displays the hit count by using Axum, Tokio, Rust.
-//! 
+//!
 //! This is a very simple web service that we use for testing our systems.
-//! 
+//!
 //! ## Steps
-//! 
-//! Run the service on host 0.0.0.0 port 3000 or wherever you wish:
-//! 
+//!
+//! Run the service using the default address 0.0.0.0:8080:
+//!
 //! ```sh
-//! cargo run -- "0.0.0.0:3000"
+//! cargo run
 //! ```
-//! 
-//! Browse <https://localhost:3000>
-//! 
+//!
+//! Browse <https://localhost:8080/count>
+//!
 //! You should see a web page that displays the hit count.
-//! 
+//!
 //! Use your browser to reload the web page.
-//! 
+//!
 //! You should see the hit count increase by one.
-//! 
+//!
+//! ## Options
+//!
+//! Run the service using a command line option for a custom address:
+//!
+//! ```sh
+//! cargo run -- "1.2.3.4:5678"
+//! ```
+//!
+//! Run the service using an environment variable for a custom address:
+//! ```sh
+//! export ADDRESS="1.2.3.4:5678"
+//! cargo run
+//! ```
+//!
 //! ## References
-//! 
+//!
 //! Based on Demo Rust Axum free open source software:
 //! <https://github.com/joelparkerhenderson/demo-rust-axum>
-//! 
+//!
 
 mod app;
 
 /// Use tracing crates for application-level tracing output.
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
-/// The main function does these steps: 
+/// The main function does these steps:
 /// - Start tracing and emit a tracing event.
 /// - Get a command line argument as our bind address.
 /// - Build our application by creating our router.
 /// - Run our application as a hyper server.
-#[tokio::main]  
+#[tokio::main]
 async fn main() {
     // Start tracing and emit a tracing event.
     tracing_subscriber::registry()
@@ -57,17 +71,21 @@ async fn main() {
     // Get command line arguments.
     let args: Vec<String> = std::env::args().skip(1).collect();
 
-    // Use the first arg for tokio::net::TcpListener::bind(…)  
-    let bind_address = match args.get(0) {
+    // Use the first arg for tokio::net::TcpListener::bind(…),
+    // or then env var ADDRESS, or default "0.0.0.0:8080".
+    let bind_address_string = match args.get(0) {
         Some(x) => x.clone(),
-        None => "0.0.0.0:3000".into(),
+        None => match std::env::var("ADDRESS") {
+            Ok(address) => address,
+            Err(_e) => "0.0.0.0:8080".into()
+        }
     };
 
     // Create our application which is an axum router.
     let app = crate::app::app();
 
     // Run our application as a hyper server.
-    let listener = tokio::net::TcpListener::bind(bind_address).await.unwrap();
+    let listener = tokio::net::TcpListener::bind(bind_address_string).await.unwrap();
     axum::serve(listener, app)
         .with_graceful_shutdown(shutdown_signal())
         .await
